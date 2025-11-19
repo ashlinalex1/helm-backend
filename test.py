9ef1f64171b972994a158c6e2ec90e833f5da629
ashlinalex1 <ashlinalex12@gmail.com>
2025-11-19 22:38:50 +0530

    remove python version pinning

--- test.py
+++
@@ -1,8 +0,0 @@
-import onnxruntime as ort
-import numpy as np
-from PIL import Image
-
-session = ort.InferenceSession("best1.onnx")
-input_name = session.get_inputs()[0].name
-print("Inputs:", session.get_inputs())
-print("Outputs:", session.get_outputs())
