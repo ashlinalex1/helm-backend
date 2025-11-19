2e10f17e459da5287c2f027404ded8f44fa705ac
ashlinalex1 <ashlinalex12@gmail.com>
2025-11-19 22:20:04 +0530

    remove python version pinning

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080"
+        "https://streetsmart-road.vercel.app",
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
